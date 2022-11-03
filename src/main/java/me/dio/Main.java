51b7148d3cd527a469b4363a19720593f624ad6e
lucaskaspary <lucaskaspary@hotmail.com>
2022-11-03 14:50:13 -0300

    Added classes and interfaces

--- src/main/java/me/dio/Main.java
+++ src/main/java/me/dio/Main.java
@@ -1,7 +1,34 @@
 package me.dio;
 
+
+import me.dio.desafio.dominio.Curso;
+import me.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descriçao da mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+
     }
 }
